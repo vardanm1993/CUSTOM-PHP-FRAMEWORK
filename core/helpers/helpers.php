8adfdecdbd48565b9ca2726cf1f9277f7e3ac88e
vardanm1993 <vardanm1993@gmail.com>
2024-08-05 18:58:32 +0400

    Add main index logic, created helpers for debug and getting path

--- core/helpers/helpers.php
+++ core/helpers/helpers.php
@@ -1,1 +1,96 @@
 <?php
+
+if (!function_exists('base_path')) {
+    function base_path(string $path = ''): string
+    {
+        return BASE_PATH . ($path ? DIRECTORY_SEPARATOR . $path : $path);
+    }
+}
+
+if (!function_exists('dd')) {
+    /**
+     * Dump the passed variables and end the script.
+     *
+     * @param mixed ...$args
+     * @return void
+     */
+    function dd(...$args)
+    {
+        echo '<style>
+            body {
+                background-color: #f6f7f8;
+                color: #c5c8c6;
+                font-family: Arial, sans-serif;
+                margin: 0;
+                padding: 0;
+            }
+            .dd-output {
+                background-color: #1d1f21;
+                color: #d9e6dd;
+                padding: 20px;
+                margin: 20px;
+                border-radius: 10px;
+                box-shadow: 0 0 10px rgba(0, 0, 0, 0.5);
+            }
+            pre {
+                background-color: #0f0f10;
+                color: #0aef58;
+                padding: 10px;
+                border-radius: 5px;
+                overflow: auto;
+            }
+        </style>';
+
+        foreach ($args as $arg) {
+            echo '<div class="dd-output"><pre>';
+            echo htmlspecialchars(print_r($arg, true));
+            echo '</pre></div>';
+        }
+
+        die(1);
+    }
+}
+
+if (!function_exists('dump')) {
+    /**
+     * Dump the passed variables without ending the script.
+     *
+     * @param mixed ...$args
+     * @return void
+     */
+    function dump(...$args)
+    {
+        echo '<style>
+            body {
+                background-color: #f6f7f8;
+                color: #333;
+                font-family: Arial, sans-serif;
+                margin: 0;
+                padding: 0;
+            }
+            .dump-output {
+                background-color: #fff;
+                color: #333;
+                padding: 20px;
+                margin: 20px;
+                border: 1px solid #ccc;
+                border-radius: 5px;
+                box-shadow: 0 0 10px rgba(0, 0, 0, 0.1);
+            }
+            pre {
+                background-color: #f9f9f9;
+                color: #333;
+                padding: 10px;
+                border-radius: 5px;
+                overflow: auto;
+            }
+        </style>';
+
+        foreach ($args as $arg) {
+            echo '<div class="dump-output"><pre>';
+            var_dump($arg);
+            echo '</pre></div>';
+        }
+    }
+}
+
--- public/index.php
+++ public/index.php
@@ -1,3 +1,23 @@
 <?php
+declare(strict_types=1);
 
-echo "Hello World!";
+use Core\Kernel;
+use Core\Request;
+
+define('BASE_PATH', dirname(__DIR__));
+
+require_once BASE_PATH . '/vendor/autoload.php';
+
+$dotenv = Dotenv\Dotenv::createImmutable(BASE_PATH);
+$dotenv->safeLoad();
+
+require base_path('web.php');
+
+
+$kernel = new Kernel();
+
+$request = new Request();
+
+$response = $kernel->handle(request: $request);
+
+$response->send();
